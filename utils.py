982adb284ce245cda5d0e0d713af9946e6aafa39
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-02 11:22:19 +0200

    rjatujte

--- utils.py
+++ utils.py
@@ -7,3 +7,23 @@ def fact_rec(number: int):
 
     else:
         return 1
+
+
+def is_prime(number: int):
+    from math import ceil
+
+    number = abs(number)
+
+    if number == 0 or number == 1:
+        return False
+
+    elif number == 2:
+        return True
+
+    end = ceil(number ** 0.5) + 1
+
+    for i in range(2, end):
+        if number % 2 == 0:
+            return False
+
+    return True
