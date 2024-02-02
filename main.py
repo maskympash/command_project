4bf9a596d19364555194799b5bc84b35a68c8e1a
Paschenko Maksym <maksympash@knu.ua>
2024-02-02 11:46:07 +0200

    Edit main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-def fact_rec(number: int):
-    if number < 0:
-        return None
+from utils import nsd
 
-    if number > 0:
-        return number * fact_rec(number - 1)
-
-    else:
-        return 1
-
-print(fact_rec())
+print(nsd(13,17))
